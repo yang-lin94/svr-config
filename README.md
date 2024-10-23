818f441eb8d41ef3249e4245769cea4e8a4235fe
yang-lin94 <casongg@gmail.com>
2024-10-23 20:34:29 +0800

    updata

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 # alpine system.bigred
 
 ```bash
-wget -qO - https://raw.githubusercontent.com/yang-lin94/srv_config/refs/heads/main/alpine/system.bigred/system.bigred.sh | sudo bash
+wget -qO - https://raw.githubusercontent.com/yang-lin94/svr-config/refs/heads/main/alpine/system.bigred/system.bigred.txt | sudo bash
 ```
 
--- list.sh
+++ list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RAW='https://raw.githubusercontent.com/yang-lin94/srv_config/refs/heads/main'
+RAW='https://raw.githubusercontent.com/yang-lin94/svr-config/refs/heads/main'
 
 echo '' >README.md
 for model in $(ls -d */ | sed 's|/||g'); do
@@ -9,7 +9,7 @@ for model in $(ls -d */ | sed 's|/||g'); do
     echo "# ${model} ${name}" >>README.md
     echo '' >>README.md
     echo '```bash' >>README.md
-    echo "wget -qO - ${RAW}/${model}/${name}/${name}.sh | sudo bash" >>README.md
+    echo "wget -qO - ${RAW}/${model}/${name}/${name}.txt | sudo bash" >>README.md
     echo '```' >>README.md
     echo '' >>README.md
   done <${model}/model_name
