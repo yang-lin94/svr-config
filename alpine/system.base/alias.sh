5e20c1a705096608ccb633b51f35023ba6bdeb89
yang-lin94 <casongg@gmail.com>
2024-11-16 11:51:31 +0800

    update

---
+++ alpine/system.base/alias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+alias ping='ping -c 4 '
+alias pingdup='sudo arping -D -I eth0 -c 2 '
+alias ll='ls -alh '
+alias poweroff='sudo poweroff; sleep 5'
+alias reboot='sudo reboot; sleep 5'
+alias kg='kubectl get'
+alias ka='kubectl apply'
+alias kd='kubectl delete'
+alias kc='kubectl create'
+alias ks='kubectl get pods -n kube-system'
+alias pc='sudo podman system prune -a -f'
