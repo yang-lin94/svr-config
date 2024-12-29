d10b249d2dbc1d2adb207026a1609ab7ea03a469
yang-lin94 <casongg@gmail.com>
2024-12-29 21:41:54 +0800

    fix

--- ubuntu/system.base/motd.sh
+++ ubuntu/system.base/motd.sh
@@ -6,16 +6,16 @@ echo "Welcome to Ubuntu Linux: $(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release 
 export SHELL='/bin/bash'
 
 # network interface
-export GW=$(ip route | awk '/default/ {print $3}')
-export GWIF=$(ip route | awk '/default/ {print $5}')
-export IP=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f1)
-export MAC=$(ip a show "${GWIF}" | awk '/ether/ {print $2}')
-export NETMASK=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f2)
+# export GW=$(ip route | awk '/default/ {print $3}')
+# export GWIF=$(ip route | awk '/default/ {print $5}')
+# export IP=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f1)
+# export MAC=$(ip a show "${GWIF}" | awk '/ether/ {print $2}')
+# export NETMASK=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f2)
 
-if [ "${IP}" != "" ]; then
-  echo "
-${GWIF} IP: ${IP}/${NETMASK}
-${GWIF} GW: ${GW}
-"
-fi
+# if [ "${IP}" != "" ]; then
+#   echo "
+# ${GWIF} IP: ${IP}/${NETMASK}
+# ${GWIF} GW: ${GW}
+# "
+# fi
 
