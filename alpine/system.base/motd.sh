1cfbaf9331dd5d7a6bddce74542b6507a8734f36
yang-lin94 <casongg@gmail.com>
2025-02-02 04:33:09 +0000

    fix

--- alpine/system.base/motd.sh
+++ alpine/system.base/motd.sh
@@ -6,11 +6,20 @@ echo "Welcome to Alpine Linux : `cat /etc/alpine-release`"
 export SHELL='/bin/bash'
 
 # network interface
-export GW=$(ip route | awk '/default/ {print $3}')
-export GWIF=$(ip route | awk '/default/ {print $5}')
-export IP=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f1)
-export MAC=$(ip a show "${GWIF}" | awk '/ether/ {print $2}')
-export NETMASK=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f2)
+export GW
+GW=$(ip route | awk '/default/ {print $3}')
+
+export GWIF
+GWIF=$(ip route | awk '/default/ {print $5}')
+
+export IP
+IP=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f1)
+
+export MAC
+MAC=$(ip a show "${GWIF}" | awk '/ether/ {print $2}')
+
+export NETMASK
+NETMASK=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f2)
 
 if [ "${IP}" != "" ]; then
   echo "
