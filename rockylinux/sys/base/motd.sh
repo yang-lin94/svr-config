6a6052a067e83d90091268ebae6be2619865323d
yang-lin94 <casongg@gmail.com>
2025-04-25 10:20:29 +0000

    feat(shell): Added basic system configuration scripts - Added alias.sh, base.txt and motd.sh scripts to set up system aliases, basic configuration and display welcome message.

---
+++ rockylinux/sys/base/motd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "Welcome to Rocky Linux: $(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '\"')"
+
+# default shell
+export SHELL='/bin/bash'
+
+# network interface
+# export GW=$(ip route | awk '/default/ {print $3}')
+# export GWIF=$(ip route | awk '/default/ {print $5}')
+# export IP=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f1)
+# export MAC=$(ip a show "${GWIF}" | awk '/ether/ {print $2}')
+# export NETMASK=$(ip a show "${GWIF}" | awk '/inet / {print $2}' | cut -d'/' -f2)
+
+# if [ "${IP}" != "" ]; then
+#   echo "
+# ${GWIF} IP: ${IP}/${NETMASK}
+# ${GWIF} GW: ${GW}
+# "
+# fi
+
